0779b7813e65c8c2b835a3a0b70979651bca677a
zkr911 <63081032+zkr911@users.noreply.github.com>
2024-08-10 02:21:29 -0400

    tarea de calcular raiz

---
+++ tareamath.py
@@ -0,0 +1,30 @@
+## 
+# 
+# 
+# El objetivo de la presente actividad es realizar un cálculo aritmético sencillo, haciendo uso de
+#variables, operaciones aritméticas, y de la función print(). Para ello requerimos importar el
+#módulo math.
+#ENUNCIADO DEL PROBLEMA:
+#Requerimos hacer el cálculo de la raíz cuadrada de una variable llamada “y” que tiene valor 81,
+#utilizando el módulo de la librería math incorporada en Python. El resultado debemos asignarlo a
+#una variable que será impresa en pantalla.
+# 
+# 
+import math
+
+
+
+
+variableY = 81
+
+raizCuadrada = math.sqrt(variableY)
+
+print("el resultado es : ",round( raizCuadrada))
+
+
+
+
+
+
+
+

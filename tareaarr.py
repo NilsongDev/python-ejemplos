ee4b77a02d5187c6c4f31670cfb31b8d282cf82b
zkr911 <63081032+zkr911@users.noreply.github.com>
2024-08-11 01:50:53 -0400

    tarea de arreglos

---
+++ tareaarr.py
@@ -0,0 +1,17 @@
+# variables asignadas 
+num1 = 8
+num2 = 10.5
+palabra = "ejercicio"
+
+# Crear diccionario
+mi_set = {num1, num2, palabra}
+
+# Crear una lista que contiene todo y el valor false
+mi_lista = [mi_set, False]
+
+# Imprimir
+print("Contenido de mi_lista:")
+print(f"mi_lista = {mi_lista}")
+
+print("\nContenido de mi_set:")
+print(f"mi_set = {list(mi_set)}")  # Convertir el set a lista para una vista tipo array

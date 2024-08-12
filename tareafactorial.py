db9b5438c403bba50d1bf954971e44378ca9c350
zkr911 <63081032+zkr911@users.noreply.github.com>
2024-08-11 23:48:55 -0400

    tarea de numeros factorial

---
+++ tareafactorial.py
@@ -0,0 +1,8 @@
+# Solicitar datos de tres personas
+numeroFactorial = input("Número factorial: ")
+
+contador = int(numeroFactorial)
+
+while contador > 0:
+    print(f"Los números son: {contador}")
+    contador -= 1
